0b673d471e24684fb4bfd6d75717637cf352355d
Billy Joe <billyjoersantos@outlook.com>
2022-07-28 21:48:05 -0300

    feature(tdd): Studying E2E with .NET
    Realizando teste de preenchimento

--- Alura.ByteBank.WebApp.Testes/AposRealizarLogin.cs
+++ Alura.ByteBank.WebApp.Testes/AposRealizarLogin.cs
@@ -48,5 +48,45 @@ namespace Alura.ByteBank.WebApp.Testes
             Assert.Contains("The Email field is required.", driver.PageSource);
             Assert.Contains("The Senha field is required.", driver.PageSource);
         }
+
+        [Fact]
+        public void RealizaLoginAcessaMenuECadastraCliente()
+        {
+            // Arrange
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+            driver.Navigate().GoToUrl("https://localhost:44309/UsuarioApps/Login");
+            var login = driver.FindElement(By.Name("Email"));
+            var senha = driver.FindElement(By.Name("Senha"));
+
+            login.SendKeys("andre@email.com");
+            senha.SendKeys("senha01");
+
+            driver.FindElement(By.CssSelector("#btn-logar")).Click();
+
+            driver.FindElement(By.LinkText("Cliente")).Click();
+
+            driver.FindElement(By.LinkText("Adicionar Cliente")).Click();
+
+            driver.FindElement(By.Name("Identificador")).Click();
+            driver.FindElement(By.Name("Identificador"))
+                .SendKeys("2df71922-ca7d-4d43-b142-0767b32f8f22a");
+
+            driver.FindElement(By.Name("CPF")).Click();
+            driver.FindElement(By.Name("CPF")).SendKeys("");
+            
+            driver.FindElement(By.Name("Nome")).Click();
+            driver.FindElement(By.Name("Nome")).SendKeys("Tobey Garfield");
+
+            driver.FindElement(By.Name("Profissao")).Click();
+            driver.FindElement(By.Name("Profissao")).SendKeys("Cientista");
+
+            // Act
+            driver.FindElement(By.CssSelector(".btn-primary")).Click();
+            driver.FindElement(By.LinkText("Home")).Click();
+            
+            // Assert
+            Assert.Contains("Logout", driver.PageSource);
+        }
     }
 }
