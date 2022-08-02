dea1307976eb8df5fb976d0fc310fdd704fa29bb
Billy Joe <billyjoersantos@outlook.com>
2022-08-01 22:10:24 -0300

    feature(tdd): Studying E2E with .NET
    Utilizando Page Objects para navegação na home

--- Alura.ByteBank.WebApp.Testes/AposRealizarLogin.cs
+++ Alura.ByteBank.WebApp.Testes/AposRealizarLogin.cs
@@ -1,5 +1,6 @@
 ﻿using Xunit;
 
+using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -11,15 +12,22 @@ using Alura.ByteBank.WebApp.Testes.PageObjects;
 
 namespace Alura.ByteBank.WebApp.Testes
 {
-    public class AposRealizarLogin
+    public class AposRealizarLogin : IDisposable
     {
+        private IWebDriver _driver;
+
+        public AposRealizarLogin()
+        {
+            _driver = new ChromeDriver(
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+            );
+        }
 
         [Fact]
         public void AposRealizarLoginVerificaSeExisteOpcaoAgenciaMenu()
         {
             // Arrange
-            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
-            var loginPo = new LoginPO(driver);
+            var loginPo = new LoginPO(_driver);
             loginPo.Navegar("https://localhost:44309/UsuarioApps/Login");
 
             // Act
@@ -27,76 +35,77 @@ namespace Alura.ByteBank.WebApp.Testes
             loginPo.ClicarBotaoLogar();
 
             // Assert
-            Assert.Contains("Agência", driver.PageSource);
+            Assert.Contains("Agência", _driver.PageSource);
         }
 
         [Fact]
         public void TentaRealizarLoginSemPreencherCampos()
         {
             // Arrange
-            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
-            var loginPo = new LoginPO(driver);
+            var loginPo = new LoginPO(_driver);
             loginPo.Navegar("https://localhost:44309/UsuarioApps/Login");
 
             // Act
             loginPo.ClicarBotaoLogar();
 
             // Assert
-            Assert.Contains("The Email field is required.", driver.PageSource);
-            Assert.Contains("The Senha field is required.", driver.PageSource);
+            Assert.Contains("The Email field is required.", _driver.PageSource);
+            Assert.Contains("The Senha field is required.", _driver.PageSource);
         }
 
         [Fact]
         public void RealizaLoginAcessaMenuECadastraCliente()
         {
             // Arrange
-            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
-            var loginPo = new LoginPO(driver);
+            var loginPo = new LoginPO(_driver);
+            var homePo = new HomePO(_driver);
+
             loginPo.Navegar("https://localhost:44309/UsuarioApps/Login");
 
             // Act
             loginPo.PreencherCamposELogar("andre@email.com", "senha01");
             loginPo.ClicarBotaoLogar();
 
-            driver.FindElement(By.LinkText("Cliente")).Click();
+            _driver.FindElement(By.LinkText("Cliente")).Click();
 
-            driver.FindElement(By.LinkText("Adicionar Cliente")).Click();
+            _driver.FindElement(By.LinkText("Adicionar Cliente")).Click();
 
-            driver.FindElement(By.Name("Identificador")).Click();
-            driver.FindElement(By.Name("Identificador"))
+            _driver.FindElement(By.Name("Identificador")).Click();
+            _driver.FindElement(By.Name("Identificador"))
                 .SendKeys("2df71922-ca7d-4d43-b142-0767b32f8f22a");
 
-            driver.FindElement(By.Name("CPF")).Click();
-            driver.FindElement(By.Name("CPF")).SendKeys("");
+            _driver.FindElement(By.Name("CPF")).Click();
+            _driver.FindElement(By.Name("CPF")).SendKeys("");
 
-            driver.FindElement(By.Name("Nome")).Click();
-            driver.FindElement(By.Name("Nome")).SendKeys("Tobey Garfield");
+            _driver.FindElement(By.Name("Nome")).Click();
+            _driver.FindElement(By.Name("Nome")).SendKeys("Tobey Garfield");
 
-            driver.FindElement(By.Name("Profissao")).Click();
-            driver.FindElement(By.Name("Profissao")).SendKeys("Cientista");
+            _driver.FindElement(By.Name("Profissao")).Click();
+            _driver.FindElement(By.Name("Profissao")).SendKeys("Cientista");
 
             // Act
-            driver.FindElement(By.CssSelector(".btn-primary")).Click();
-            driver.FindElement(By.LinkText("Home")).Click();
+            _driver.FindElement(By.CssSelector(".btn-primary")).Click();
+            homePo.LinkHomeClick();
 
             // Assert
-            Assert.Contains("Logout", driver.PageSource);
+            Assert.Contains("Logout", _driver.PageSource);
         }
 
         [Fact]
         public void RealizaLoginAcessaListagemDeContas()
         {
             // Arrange
-            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
-            var loginPo = new LoginPO(driver);
+            var loginPo = new LoginPO(_driver);
+            var homePo = new HomePO(_driver);
+
             loginPo.Navegar("https://localhost:44309/UsuarioApps/Login");
 
             // Act
             loginPo.PreencherCamposELogar("andre@email.com", "senha01");
             loginPo.ClicarBotaoLogar();
             
-            driver.FindElement(By.Id("contacorrente")).Click();
-            IReadOnlyCollection<IWebElement> elements = driver.FindElements(By.TagName("a"));
+            homePo.LinkContaCorrenteClick();
+            IReadOnlyCollection<IWebElement> elements = _driver.FindElements(By.TagName("a"));
 
             var elemento = (from webElemento in elements
                             where webElemento.Text.Contains("Detalhes")
@@ -106,7 +115,12 @@ namespace Alura.ByteBank.WebApp.Testes
             elemento.Click();
 
             // Assert
-            Assert.Contains("Voltar", driver.PageSource);
+            Assert.Contains("Voltar", _driver.PageSource);
+        }
+
+        public void Dispose()
+        {
+            _driver.Quit();
         }
     }
 }
---
+++ Alura.ByteBank.WebApp.Testes/PageObjects/HomePO.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+
+namespace Alura.ByteBank.WebApp.Testes.PageObjects
+{
+    public class HomePO
+    {
+        private IWebDriver _driver;
+        private By _linkHome;
+        private By _linkContaCorrente;
+        private By _linkClientes;
+        private By _linkAgencia;
+
+        public HomePO(IWebDriver driver)
+        {
+            _driver = driver;
+
+            _linkHome = By.Id("home");
+            _linkContaCorrente = By.Id("contacorrente");
+            _linkClientes = By.Id("clientes");
+            _linkAgencia = By.Id("agencia");
+        }
+
+        public void Navegar(string url)
+        {
+            _driver.Navigate().GoToUrl(url);
+        }
+
+        public void LinkHomeClick()
+        {
+            _driver.FindElement(_linkHome).Click();
+        }
+
+        public void LinkContaCorrenteClick()
+        {
+            _driver.FindElement(_linkContaCorrente).Click();
+        }
+
+        public void LinkClientesClick()
+        {
+            _driver.FindElement(_linkClientes).Click();
+        }
+
+        public void LinkAgenciasClick()
+        {
+            _driver.FindElement(_linkAgencia).Click();
+        }
+    }
+}
